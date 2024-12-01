ae7be7b72f7c402fccc00e9a510324893e0c132b
Romain Touchet <romain.touchet@gmail.com>
2024-12-01 11:42:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code - Lunesombre
+
+## Advent of Code 2024
+ It's been a few years that I'm interested in the Advent of code but don't have the time to try it.
+
+ This year, I still don't have time, but I will give it a shot nonetheless! 
---
+++ src/main/java/adventofcode/aoc2024/AdventOfCodeApplication.java
@@ -0,0 +1,15 @@
+package adventofcode.aoc2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdventOfCodeApplication.class, args);
+
+    System.out.println("Let's go !");
+  }
+
+}
